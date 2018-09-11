62e3059270ef4696b532d65b4f3bb110def0e844
Fredrik Rönnehag <fredrik.ronnehag@gmail.com>
2018-09-11 13:39:08 +0200

    Påbörjade storyn

--- FredrikSebastianEllen/Program.cs
+++ FredrikSebastianEllen/Program.cs
@@ -10,12 +10,22 @@ namespace FredrikSebastianEllen
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            string förstaMeddelande = "En liten rolig filur som hette ";
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Console.WriteLine(förstaMeddelande);
+
+            Console.Write("Fortsätt storyn...");
+            string användarSvar = Console.ReadLine();
+
+
+            // Fortsätt historien här under
+
+            // Lägg till variablerna i WriteLine.
+
+
+            Console.WriteLine($"{förstaMeddelande} {användarSvar}");
+
+            Console.ReadLine();
         }
     }
 }
